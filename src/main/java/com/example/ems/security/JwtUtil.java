48aa33bfe7b4bfc9fd82c5b065a960b8ab416699
prathameshwadekar724 <157518241+prathameshwadekar724@users.noreply.github.com>
2026-01-02 22:02:25 +0530

    updated utils

--- src/main/java/com/example/ems/security/JwtUtil.java
+++ src/main/java/com/example/ems/security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -12,8 +13,8 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final String SECRET_KEY =
-            "ems_secret_key_ems_secret_key_32chars";
+    @Value("${JWT_SECRET}")
+    private String SECRET_KEY;
     private final long EXPIRATION=1000*60*60;
 
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
