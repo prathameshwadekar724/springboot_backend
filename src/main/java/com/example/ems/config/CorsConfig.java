1930500f01a0c6d05772d1cbed16551426de8b93
prathameshwadekar724 <157518241+prathameshwadekar724@users.noreply.github.com>
2026-01-03 14:54:30 +0530

    added frontend origin

--- src/main/java/com/example/ems/config/CorsConfig.java
+++ src/main/java/com/example/ems/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://ems-frontend-gamma-gilt.vercel.app")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
